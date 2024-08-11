a9854bbf057ce5e2e4fc45a89e38087b139c280b
SeaStove <rochovian@gmail.com>
2024-08-11 19:11:35 -0400

    removing shrooms section

--- Options.lua
+++ Options.lua
@@ -4,15 +4,6 @@ GroundTargetMacros.defaults = {
 	targetMacros = false
 }
 
-local function CreateIcon(icon, width, height, parent)
-	local f = CreateFrame("Frame", nil, parent)
-	f:SetSize(width, height)
-	f.tex = f:CreateTexture()
-	f.tex:SetAllPoints(f)
-	f.tex:SetTexture(icon)
-	return f
-end
-
 function GroundTargetMacros:CreateCheckbox(option, label, parent, updateFunc)
 	local cb = CreateFrame("CheckButton", nil, parent, "UICheckButtonTemplate")
 	cb.Text:SetText(label)
@@ -57,18 +48,6 @@ function GroundTargetMacros:InitializeOptions()
 
 	local category = Settings.RegisterCanvasLayoutCategory(self.panel_main, "GroundTargetMacros")
 	Settings.RegisterAddOnCategory(category)
-
-	-- sub panel
-	local panel_shroom = CreateFrame("Frame")
-	panel_shroom.name = "Shrooms"
-	panel_shroom.parent = self.panel_main.name
-
-	for i = 1, 10 do
-		local icon = CreateIcon("interface/icons/inv_mushroom_11", 32, 32, panel_shroom)
-		icon:SetPoint("TOPLEFT", 20, -32 * i)
-	end
-
-	local shroomCategory = Settings.RegisterCanvasLayoutSubcategory(category, panel_shroom, "Shrooms")
 end
 
 -- a bit more efficient to register/unregister the event when it fires a lot
